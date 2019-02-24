2075439d09382c91182a58a1704800766577b1fa
Lydia Febtriani <lydiafebtriani3@gmail.com>
2019-02-24 08:02:17 +0000

    Delete the Class migration and add the ClassRoom migration

---
+++ database/migrations/2019_02_24_054826_create_class_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classrooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('teacher_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::table('classrooms', function($table) {
+            $table->foreign('teacher_id')->references('id')->on('teachers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('class_rooms');
+    }
+}
